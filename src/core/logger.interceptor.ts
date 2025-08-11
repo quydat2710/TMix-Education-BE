c39e276baf3b3d2d52b32dc6e8c91be180997a1a
quydat2710 <quydat2710@gmail.com>
2025-08-11 07:49:52 +0700

    update logger

--- src/core/logger.interceptor.ts
+++ src/core/logger.interceptor.ts
@@ -7,13 +7,12 @@ export class HttpLoggerInterceptor implements NestInterceptor {
     private logger = new Logger(HttpLoggerInterceptor.name)
 
     intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> | Promise<Observable<any>> {
-        const start = Date.now();
+        const start = Date.now()
         const req = context.switchToHttp().getRequest()
         const res = context.switchToHttp().getResponse()
         const { name, email } = req?.user
         const statusCode = res.statusCode
         const { method, originalUrl } = req;
-        let loged = false
 
         return next.handle().pipe(
             tap(() => {
@@ -21,7 +20,6 @@ export class HttpLoggerInterceptor implements NestInterceptor {
                 this.logger.log(
                     `${name}:${email} ${method} ${originalUrl} ${statusCode} - ${duration}ms`
                 );
-                loged = true;
             }),
             catchError((error) => {
                 const duration = Date.now() - start;
