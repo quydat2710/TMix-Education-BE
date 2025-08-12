1510fec8048cf5566ae249bea18bd5f8d62834aa
quydat2710 <quydat2710@gmail.com>
2025-08-12 21:22:35 +0700

    modify main

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { AllConfigType } from './config/config.type';
 import { I18nValidationExceptionFilter, I18nValidationPipe } from 'nestjs-i18n';
-import { ClassSerializerInterceptor } from '@nestjs/common';
+import { ClassSerializerInterceptor, VersioningType } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     bufferLogs: true
@@ -20,9 +22,20 @@ async function bootstrap() {
       },
     }),
   );
+
+  //config cookie
+  app.use(cookieParser())
+
   app.useGlobalInterceptors(
     new ClassSerializerInterceptor(app.get(Reflector))
   )
+
+  //config versioning
+  app.enableVersioning({
+    type: VersioningType.URI,
+    prefix: 'api/v',
+    defaultVersion: ['1']
+  })
   await app.listen(configService.get('app.port', { infer: true }));
 }
 bootstrap();
