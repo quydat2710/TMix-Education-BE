0f211a40d59a9b4458771d62775ca856d78aac7d
quydat2710 <quydat2710@gmail.com>
2025-08-26 13:40:56 +0700

    add delete file service

--- src/modules/files/files.module.ts
+++ src/modules/files/files.module.ts
@@ -1,10 +1,12 @@
 import { Global, Module } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { MulterModule } from '@nestjs/platform-express';
+import { FilesController } from './files.controller';
 
 @Global()
 @Module({
   imports: [MulterModule.register({})],
+  controllers: [FilesController],
   providers: [FilesService],
   exports: [FilesService]
 })
