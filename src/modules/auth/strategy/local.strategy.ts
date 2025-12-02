8a439b12b2fd56955b9366a5704eedd1f3e9ee6f
quydat2710 <quydat2710@gmail.com>
2025-12-02 17:07:29 +0700

    fix(auth):change status code if incorrect username or password

--- src/modules/auth/strategy/local.strategy.ts
+++ src/modules/auth/strategy/local.strategy.ts
@@ -1,6 +1,6 @@
 import { Strategy } from 'passport-local';
 import { PassportStrategy } from '@nestjs/passport';
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
 import { AuthService } from 'modules/auth/auth.service';
 import { I18nService } from 'nestjs-i18n';
 import { I18nTranslations } from '@/generated/i18n.generated';
@@ -20,7 +20,7 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
     async validate(email: string, password: string): Promise<any> {
         const user = await this.authService.validateUser(email, password);
         if (!user) {
-            throw new UnauthorizedException(this.i18nService.t('auth.INCORRECT'));
+            throw new BadRequestException(this.i18nService.t('auth.INCORRECT'));
         }
         return user;
     }
